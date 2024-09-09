e0fa649c2f7dcd029c494e072d8d3c750b9dfd4b
alex2000d <demarcoalessandro777@gmail.com>
2024-09-09 16:02:33 +0200

    [UPD] imported the cdn vue

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <title>Document</title>
 </head>
 <body>
